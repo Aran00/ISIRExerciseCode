7ba5089ea837069477ac58427bda96c775c782d7
Aran <yuran00@gmail.com>
2015-10-14 14:21:47 +0800

    Add some plots

--- chapter03/AutoExec.py
+++ chapter03/AutoExec.py
@@ -9,6 +9,7 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from scipy.interpolate import spline
 
+import statsmodels.graphics.regressionplots as rp
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 class AutoExec:
@@ -17,7 +18,7 @@ class AutoExec:
         self.df = auto.dropna()
 
     def simple_regession(self):
-        ''' The answer of exercise08:
+        ''' The answer of exercise03-08:
         (a) (i)有 (ii)|t|值很大,p值很小,相关性强 (iii)负相关 (iv)预测区间如何计算?
         (b) 已完成
         (c) Residual/fitted 似乎是一个二次曲线 '''
@@ -26,6 +27,7 @@ class AutoExec:
         y = self.df['mpg']
         X = self.df[['horsepower']]
         X = sm.add_constant(X)
+        print X
         res = sm.OLS(y, X).fit()
         # res = model.fit()
         print res.summary()
@@ -33,10 +35,15 @@ class AutoExec:
         print "The prediction is: ", res.predict(exog=[[1, 98]])
         print "The prediction interval is: "
 
+        '''
         self.df.plot(kind="scatter", x='horsepower', y='mpg', c='w')
         graph_x = np.linspace(min(self.df['horsepower']), 200)
         graph_y = res.predict(sm.add_constant(graph_x))
         plt.plot(graph_x, graph_y)
+        '''
+        fig = rp.abline_plot(model_results=res)
+        ax = fig.axes[0]
+        ax.scatter(X['horsepower'], y, c='w')
         plt.show()
 
     def plot_scatter_matrix(self):
@@ -52,7 +59,7 @@ class AutoExec:
         print "The correlation coefficients of each column is: \n", cov_df
 
         '''
-        The answer of exercise-0-09:
+        The answer of exercise-03-09:
         (a) (b) 略
         (c) (i)有 (ii)displacement, weight, year, origin; year最相关 (iii)说明year与mpg是正相关
         '''
@@ -61,6 +68,10 @@ class AutoExec:
         y = self.df['mpg']
         self.res = sm.OLS(y, self.X).fit()
         print self.res.summary()
+
+        # The Leverage-Studentized Residuals plot
+        rp.influence_plot(self.res, criterion="DFFITS", size=20)
+        plt.show()
 
     def plot_predict_residual(self):
         ''' How to fit this type of data? '''
@@ -80,4 +91,4 @@ if __name__ == '__main__':
     #lr.simple_regession()
     lr.multi_variate_regression()
     #lr.plot_predict_residual()
-
+    #lr.test()
